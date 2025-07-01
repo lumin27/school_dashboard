6f9d7a0c31b048bfa00764d2a50f5c398756a5b3
Lu Min <linnmu2030@gmail.com>
2025-07-02 02:51:09 +0300

    update_pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -14,34 +14,41 @@ const Pagination = ({ page, total }: { page: number; total: number }) => {
   };
 
   return (
-    <div className='p-4 flex items-center justify-between text-gray-500'>
-      <button
-        onClick={() => handlePageChange(page - 1)}
-        disabled={page === 1}
-        className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold
-         disabled:opacity-50 disabled:cursor-not-allowed'>
-        Prev
-      </button>
-      <div className='flex items-center gap-2'>
-        {Array.from({ length: Math.ceil(total / ITEM_PER_PAGE) }, (_, i) => (
+    <>
+      {total === 0 ? null : (
+        <div className='p-4 flex items-center justify-between text-gray-500'>
           <button
-            onClick={() => handlePageChange(i + 1)}
-            key={i}
-            className={`${
-              page === i + 1 ? "bg-lmSky" : ""
-            } px-2 rounded-md font-semibold`}>
-            {i + 1}
-          </button>
-        ))}
-      </div>
-      <button
-        onClick={() => handlePageChange(page + 1)}
-        disabled={page >= Math.ceil(total / ITEM_PER_PAGE) || total === 0}
-        className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold
+            onClick={() => handlePageChange(page - 1)}
+            disabled={page === 1}
+            className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold
          disabled:opacity-50 disabled:cursor-not-allowed'>
-        Next
-      </button>
-    </div>
+            Prev
+          </button>
+          <div className='flex items-center gap-2'>
+            {Array.from(
+              { length: Math.ceil(total / ITEM_PER_PAGE) },
+              (_, i) => (
+                <button
+                  onClick={() => handlePageChange(i + 1)}
+                  key={i}
+                  className={`${
+                    page === i + 1 ? "bg-lmSky" : ""
+                  } px-2 rounded-md font-semibold`}>
+                  {i + 1}
+                </button>
+              )
+            )}
+          </div>
+          <button
+            onClick={() => handlePageChange(page + 1)}
+            disabled={page >= Math.ceil(total / ITEM_PER_PAGE) || total === 0}
+            className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold
+         disabled:opacity-50 disabled:cursor-not-allowed'>
+            Next
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
