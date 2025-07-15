ffac08b24406c1a30061228503c9838e29675616
Lu Min <linnmu2030@gmail.com>
2025-07-15 21:26:50 +0300

    updated_sign_file

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@
 
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
-import { ClerkProvider, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -26,52 +26,45 @@ const LoginPage = () => {
 
   return (
     <div className='h-screen flex items-center justify-center bg-lmSkyLight'>
-      <ClerkProvider
-        appearance={{
-          elements: {
-            footer: "hidden",
-          },
-        }}>
-        <SignIn.Root>
-          <SignIn.Step
-            name='start'
-            className='bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2'>
-            <h1 className='text-xl font-bold flex items-center gap-2'>
-              <Image src='/logo.png' alt='' width={24} height={24} />
-              Hnin School
-            </h1>
-            <h2 className='text-gray-400'>Sign in to your account</h2>
-            <Clerk.GlobalError className='text-sm text-red-400' />
-            <Clerk.Field name='identifier' className='flex flex-col gap-2'>
-              <Clerk.Label className='text-xs text-gray-500'>
-                Username
-              </Clerk.Label>
-              <Clerk.Input
-                type='text'
-                required
-                className='p-2 rounded-md ring-1 ring-gray-300'
-              />
-              <Clerk.FieldError className='text-xs text-red-400' />
-            </Clerk.Field>
-            <Clerk.Field name='password' className='flex flex-col gap-2'>
-              <Clerk.Label className='text-xs text-gray-500'>
-                Password
-              </Clerk.Label>
-              <Clerk.Input
-                type='password'
-                required
-                className='p-2 rounded-md ring-1 ring-gray-300'
-              />
-              <Clerk.FieldError className='text-xs text-red-400' />
-            </Clerk.Field>
-            <SignIn.Action
-              submit
-              className='bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]'>
-              Sign In
-            </SignIn.Action>
-          </SignIn.Step>
-        </SignIn.Root>
-      </ClerkProvider>
+      <SignIn.Root>
+        <SignIn.Step
+          name='start'
+          className='bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2'>
+          <h1 className='text-xl font-bold flex items-center gap-2'>
+            <Image src='/logo.png' alt='' width={24} height={24} />
+            Hnin School
+          </h1>
+          <h2 className='text-gray-400'>Sign in to your account</h2>
+          <Clerk.GlobalError className='text-sm text-red-400' />
+          <Clerk.Field name='identifier' className='flex flex-col gap-2'>
+            <Clerk.Label className='text-xs text-gray-500'>
+              Username
+            </Clerk.Label>
+            <Clerk.Input
+              type='text'
+              required
+              className='p-2 rounded-md ring-1 ring-gray-300'
+            />
+            <Clerk.FieldError className='text-xs text-red-400' />
+          </Clerk.Field>
+          <Clerk.Field name='password' className='flex flex-col gap-2'>
+            <Clerk.Label className='text-xs text-gray-500'>
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type='password'
+              required
+              className='p-2 rounded-md ring-1 ring-gray-300'
+            />
+            <Clerk.FieldError className='text-xs text-red-400' />
+          </Clerk.Field>
+          <SignIn.Action
+            submit
+            className='bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]'>
+            Sign In
+          </SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
     </div>
   );
 };
