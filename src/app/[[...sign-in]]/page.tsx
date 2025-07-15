b0a2672932477093829f964f0eafd270af818a20
Lu Min <linnmu2030@gmail.com>
2025-07-15 22:07:51 +0300

    updated_sign_in_file

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -23,6 +23,10 @@ const LoginPage = () => {
       router.replace("/");
     }
   }, [isLoaded, isSignedIn, user, router]);
+  console.log("✅ user", user);
+  console.log("✅ isSignedIn", isSignedIn);
+  console.log("✅ isLoaded", isLoaded);
+  console.log("✅ role", user?.publicMetadata?.role);
 
   return (
     <div className='h-screen flex items-center justify-center bg-lmSkyLight'>
@@ -31,11 +35,13 @@ const LoginPage = () => {
           name='start'
           className='bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2'>
           <h1 className='text-xl font-bold flex items-center gap-2'>
-            <Image src='/logo.png' alt='' width={24} height={24} />
+            <Image src='/logo.png' alt='logo' width={24} height={24} />
             Hnin School
           </h1>
           <h2 className='text-gray-400'>Sign in to your account</h2>
+
           <Clerk.GlobalError className='text-sm text-red-400' />
+
           <Clerk.Field name='identifier' className='flex flex-col gap-2'>
             <Clerk.Label className='text-xs text-gray-500'>
               Username
@@ -47,6 +53,7 @@ const LoginPage = () => {
             />
             <Clerk.FieldError className='text-xs text-red-400' />
           </Clerk.Field>
+
           <Clerk.Field name='password' className='flex flex-col gap-2'>
             <Clerk.Label className='text-xs text-gray-500'>
               Password
@@ -58,6 +65,7 @@ const LoginPage = () => {
             />
             <Clerk.FieldError className='text-xs text-red-400' />
           </Clerk.Field>
+
           <SignIn.Action
             submit
             className='bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]'>
